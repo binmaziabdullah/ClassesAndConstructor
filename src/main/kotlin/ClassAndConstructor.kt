e268146f287bc8e3e51c1429bd01a427ae93d4e6
abdullah <binmazi22@gmail.com>
2021-07-06 18:30:22 +0530

    kotlin example of classes and constructor

---
+++ src/main/kotlin/ClassAndConstructor.kt
@@ -0,0 +1,15 @@
+class ClassAndConstructor(name: String){
+
+    /*ClassAndConstructor is the header point in a class*/
+   /*this is init block at the time of object creation,
+    it will invoke the init block and following code get printed
+    on to the console*/
+
+    var names = name;
+
+
+
+
+
+
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,8 @@
+import ClassAndConstructor as ClassAndConstructor1
+
+fun main(args: Array<String>) {
+    val classAndConstructor = ClassAndConstructor1("Abdullah binmazi")
+    println("this is example of kotlin by: ${classAndConstructor.names}") //calling variable using $ sign
+    println("this is example of kotlin by:" +classAndConstructor.names) //calling variable normally as in java
+
+}
